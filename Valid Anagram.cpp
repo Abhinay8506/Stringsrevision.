446fefadf8fa2486ed5dd1254ca4f6666b11cdb9
Abhinay Goswami <goswamiabhinay26@gmail.com>
2024-07-08 19:25:02 +0530

    Create Valid Anagram.cpp

---
+++ Valid Anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       int ch[26]={0};
+       if(s.length()!=t.length()){
+           return false;
+       }
+       for(int i=0;i<s.length();i++){
+          ch[s[i]-'a']++;
+       }
+       for(int i=0;i<t.length();i++){
+          ch[t[i]-'a']--;
+       }
+       for(int i=0;i<26;i++){
+        if(ch[i]!=0){
+            return false;
+        }
+       }
+       return true;
+
+    }
+};
